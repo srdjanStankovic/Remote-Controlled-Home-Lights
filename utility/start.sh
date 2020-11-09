37fccc092d86ea66f2108c22fb2b5c86a3b22c0f
srdjanStankovic <srdjanrstankovic@gmail.com>
2020-11-09 17:57:10 +0000

    replace module name

--- utility/start.sh
+++ utility/start.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
 
 echo "------------------------------------ start.sh ----------------------------------------------"
-/usr/bin/python3 sonoff-switches.py > /home/pi/Remote-Controlled-Home-Lights/sonoff.log 2>&1
+/usr/bin/python3 the_home_gateway > /home/pi/Remote-Controlled-Home-Lights/the_home_gateway.log 2>&1
 echo "DONE"
